924ae0c47d43f250e46ccb8d5dcfde5ac0cfc5ec
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-22 16:30:07 +0200

    metodo svuota lista, numero eventi e stampa lista integrati

--- GestoreEventi/ProgrammaEventi.cs
+++ GestoreEventi/ProgrammaEventi.cs
@@ -30,7 +30,7 @@ namespace GestoreEventi
 
         }
 
-        public void CercaEvento(DateTime dataEvento)
+        /*public void CercaEvento(List<Evento> programmaEventi)
         {
 
             foreach(Evento evento in programmaEventi)
@@ -38,7 +38,7 @@ namespace GestoreEventi
                 Console.WriteLine("Inserisci una data (gg/mm/yyyy) per vedere che eventi ci saranno");
                 string dataUtente = Console.ReadLine ();
                 DateTime data = DateTime.Parse(dataUtente);
-                if (dataEvento == data)
+                if (programmaEventi.Contains(evento.dataEvento))
                 {
 
                     Console.WriteLine(data + " - " + titoloEvento);
@@ -53,19 +53,57 @@ namespace GestoreEventi
 
             }
 
-        }
+        }*/
 
-        public static void StampaEventi()
+        public static void StampaDati(List<Evento> listaEventi)
         {
 
-            foreach(Evento evento in programmaEventi)
+            foreach (Evento evento in listaEventi)
             {
 
-
+                evento.ToString();
 
             }
 
         }
 
+        public void NumeroEventi(List<Evento> listaEventi)
+        {
+            int count = 0;
+            for(int i = 0; i < listaEventi.Count; i++)
+            {
+
+                
+                count++;
+
+            }
+            Console.WriteLine("Nel softwere ci sono: " + count + " eventi");
+        }
+            
+        public void SvuotaLista(List<Evento> listaEventi)
+        {
+
+            listaEventi.Clear();
+            Console.WriteLine("Lista cancellata correttamente");
+
+        }
+
+        public void StampaLista(List<Evento> listaEventi)
+        {
+
+            foreach(Evento evento in listaEventi)
+            {
+
+                evento.ToString ();
+
+            }
+
+
+        }
     }
+
+
 }
+
+
+
