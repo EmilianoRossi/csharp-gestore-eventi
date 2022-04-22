41722fa02de47226580df47324609a324311dc5a
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-22 11:20:25 +0200

    test evento con prenota e disdici riuscito

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GestoreEventi;
+
+Console.WriteLine("------------- BENVENUTO NEL SOFTWERE EVENTI --------------");
+
+Console.WriteLine("------------ Inserisci il titolo dell'evento -------------");
+string titoloEvento = Console.ReadLine();
+
+Console.WriteLine("------- Inserisci la data dell'evento (gg/mm/yyyy) -------");
+string stringaData = Console.ReadLine();
+DateTime dataEvento = DateTime.Parse(stringaData);
+
+Console.WriteLine("---------- Inserisci il numero di posti totali -----------");
+int numeroPostiTotali = int.Parse(Console.ReadLine());
+
+Evento nuovoEvento = new Evento(titoloEvento, dataEvento, numeroPostiTotali);
+nuovoEvento.Prenota();
+nuovoEvento.Disdici();
